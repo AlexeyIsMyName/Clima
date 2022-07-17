4812e1872528420b8940a3ef6212b4527644b94c
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-18 01:12:05 +0300

    made method fetchWeather(cityName: String) in WeatherManager

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=df4e0e4b0379bb62363025175786e140&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        print(urlString)
+    }
+}

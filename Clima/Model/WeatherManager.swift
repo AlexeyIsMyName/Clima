f352f15931a7615d32d7cebb273434e340091ded
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-18 17:35:47 +0300

    made performRequest method look more "Swifty" with using a closure

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -17,26 +17,27 @@ struct WeatherManager {
     }
     
     func performRequest(urlString: String) {
+        
         //1. Create a URL
         if let url = URL(string: urlString) {
             //2. Create a URLSession
             let session = URLSession(configuration: .default)
+            
             //3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data:responce:error:))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+            }
+            
             //4. Statrt the task
             task.resume()
-        }
-    }
-    
-    func handle(data: Data?, responce: URLResponse?, error: Error?) -> Void {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
         }
     }
 }
